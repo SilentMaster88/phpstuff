ba2fe6ff4f9d1e62f1514e4448bd966a57c54365
SilentMaster88 <vale_lanza@yahoo.it>
2021-12-18 17:15:31 +0100

    Update dateconvertertousa.php

--- dateconvertertousa.php
+++ dateconvertertousa.php
@@ -5,7 +5,7 @@ $mysql_date = '2021‐12‐18';
 $euro_date = date('d/m/Y', strtotime($mysql_date));
 
 // From European date to MySQL format date:
-$euro_date = '26/12/2011';
+$euro_date = '18/12/2021';
 list($d,$m,$y) = explode('/',$euro_date);
 $mysql_date = "$y‐$m‐$d";
 ?>
