cdc956a68dd3cc485e2b7ef1ffb94ba07feed15f
SilentMaster88 <vale_lanza@yahoo.it>
2021-12-18 15:15:13 +0100

    Create arraytocsv.php

---
+++ arraytocsv.php
@@ -0,0 +1,19 @@
+<?php
+function array2CSV(array &$array)
+{
+ if (count($array) == 0) 
+ {
+  return null;
+ }
+ ob_start();
+ $df = fopen("php://output", 'w');
+ fputcsv($df, array_keys(reset($array)));
+ foreach ($array as $row) 
+ {
+  fputcsv($df, $row);
+ }
+ fclose($df);
+ return ob_get_clean();
+ }
+
+?>
